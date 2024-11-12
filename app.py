c630cafaaf4dc138af0ab133f5148d141740ebb4
Ratnesh Dubey <blazerd18@gamil.com>
2024-11-12 20:06:22 +0530

    Added all dependencies in dependencies file

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
-from flask import Flask, request, jsonify, render_template
-import requests
-import sys
-import io
-import webbrowser
+from dependencies import *
+from utilities import *
 
 # Initialize the Flask app
 app = Flask(__name__)
-
-STACK_OVERFLOW_API_URL = 'https://api.stackexchange.com/2.3/search'
-
 
 @app.route('/')
 def index():
--- dependencies.py
+++ dependencies.py
@@ -1,13 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-import requests
-import io
-import sys
-from pygments.lexers import PythonLexer
-from pygments.token import Token
 import networkx as nx
+from flask import Flask, request, jsonify, render_template
+import requests
+import sys
+import io
 import webbrowser
-from threading import Timer
 
 app = Flask(__name__)
 
-STACK_OVERFLOW_API_URL = "https://api.stackexchange.com/2.2/search?order=desc&sort=activity&site=stackoverflow"
+STACK_OVERFLOW_API_URL = 'https://api.stackexchange.com/2.3/search'
